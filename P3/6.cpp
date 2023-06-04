ae9b5a972fbec74b25a8917cdd5a1601f4d11451
Sai-Suraj-27 <sai.suraj.27.729@gmail.com>
2023-06-04 19:12:42 +0530

    Solved prev edu round problem 'A'

--- P3/6.cpp
+++ P3/6.cpp
@@ -322,7 +322,19 @@ int main()
     {
         
         ll i,j,k;
-        
+        ll x;
+        cin >> x >> k;
+
+        if(x%k != 0)
+        {
+            cout << 1 << endl;
+            cout << x << endl;
+        }
+        else
+        {
+            cout << 2 << endl;
+            cout << x-1 << " " << 1 << endl;
+        }
 
 
 
